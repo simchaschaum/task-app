c9bb2389bbc84fde40de1f8c9b51a92c8526d4c0
simchaschaum <72506778+simchaschaum@users.noreply.github.com>
2021-01-16 21:14:40 +0200

    bug fix - on mobile mode, menu closes when button is pressed.

--- src/components/header.js
+++ src/components/header.js
@@ -14,18 +14,16 @@ class Header extends React.Component {
 
     showCategory = (e) => {
         this.props.showCategory(e.target.name);
+        this.props.menuClose();
     }
 
     toggleForm = () => { 
         this.props.toggleForm();
+        this.props.menuClose();
     }
     
     toggleDeets = () => {
        this.props.toggleDeets()
-    }
-
-    deleteDone = () => {
-        alert("delete done!")
     }
 
     toggleSignIn = () => {
@@ -34,6 +32,13 @@ class Header extends React.Component {
 
     deleteDone = () => {
         this.props.deleteDone();
+        this.props.menuClose();
+    }
+
+    taskSort = (e) => {
+        e.preventDefault();
+        this.props.taskSort(e);
+        this.props.menuClose();
     }
 
     render(){
@@ -56,11 +61,10 @@ class Header extends React.Component {
                         <div className="headerItem" id="headerTop">
                             <img alt="Check-mark logo" className="logoIcon" src="https://img.icons8.com/ios/100/000000/checked-2--v2.png"/>
                                 <button id="menuClose" onClick={this.props.menuClose}>
-                                    {/* <img id="menuCloseIcon" src="https://img.icons8.com/ios-filled/100/000000/x.png"/>  */}
                                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-width="100" height="100"
-viewBox="0 0 172 172"
-style={{fill:"#000000"}}><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" ><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#6c757d"><path d="M107.33488,86l46.8872,-70.3308c0.70176,-1.05608 0.77056,-2.41144 0.172,-3.52944c-0.59856,-1.118 -1.76472,-1.81976 -3.03408,-1.81976h-25.2496c-1.12488,0 -2.18096,0.5504 -2.82424,1.47576l-37.28616,53.56424l-37.2896,-53.56424c-0.64328,-0.92536 -1.69592,-1.47576 -2.8208,-1.47576h-25.2496c-1.26936,0 -2.43552,0.69832 -3.03408,1.81632c-0.59856,1.118 -0.52976,2.4768 0.172,3.52944l46.8872,70.33424l-46.8872,70.3308c-0.70176,1.05608 -0.77056,2.41144 -0.172,3.52944c0.59856,1.118 1.76472,1.81976 3.03408,1.81976h25.2496c1.12488,0 2.18096,-0.5504 2.82424,-1.47576l37.28616,-53.56424l37.2896,53.56424c0.64328,0.92536 1.69592,1.47576 2.8208,1.47576h25.2496c1.26936,0 2.43552,-0.69832 3.03408,-1.81632c0.59856,-1.118 0.52976,-2.4768 -0.172,-3.52944z"></path></g></g></svg>
+                                        width="100" height="100"
+                                        viewBox="0 0 172 172"
+                                        style={{fill:"#000000"}}><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" ><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#6c757d"><path d="M107.33488,86l46.8872,-70.3308c0.70176,-1.05608 0.77056,-2.41144 0.172,-3.52944c-0.59856,-1.118 -1.76472,-1.81976 -3.03408,-1.81976h-25.2496c-1.12488,0 -2.18096,0.5504 -2.82424,1.47576l-37.28616,53.56424l-37.2896,-53.56424c-0.64328,-0.92536 -1.69592,-1.47576 -2.8208,-1.47576h-25.2496c-1.26936,0 -2.43552,0.69832 -3.03408,1.81632c-0.59856,1.118 -0.52976,2.4768 0.172,3.52944l46.8872,70.33424l-46.8872,70.3308c-0.70176,1.05608 -0.77056,2.41144 -0.172,3.52944c0.59856,1.118 1.76472,1.81976 3.03408,1.81976h25.2496c1.12488,0 2.18096,-0.5504 2.82424,-1.47576l37.28616,-53.56424l37.2896,53.56424c0.64328,0.92536 1.69592,1.47576 2.8208,1.47576h25.2496c1.26936,0 2.43552,-0.69832 3.03408,-1.81632c0.59856,-1.118 0.52976,-2.4768 -0.172,-3.52944z"></path></g></g></svg>
                                 </button>
                         </div>
                    
@@ -72,9 +76,9 @@ style={{fill:"#000000"}}><g fill="none" fill-rule="nonzero" stroke="none" stroke
                         </div>
                                             
                         <DropdownButton id="dropdown-basic-button"  title="Sort" className="headerItem">
-                                <Dropdown.Item name="star"  onClick={this.props.taskSort}>Starred First</Dropdown.Item>
-                                <Dropdown.Item name="addedAt"  onClick={this.props.taskSort}>Most Recent</Dropdown.Item>
-                                <Dropdown.Item name="date"  onClick={this.props.taskSort}>Due Sooner</Dropdown.Item>
+                                <Dropdown.Item name="star"  onClick={(e)=>this.taskSort(e)}>Starred First</Dropdown.Item>
+                                <Dropdown.Item name="addedAt"  onClick={(e)=>this.taskSort(e)}>Most Recent</Dropdown.Item>
+                                <Dropdown.Item name="date"  onClick={(e)=>this.taskSort(e)}>Due Sooner</Dropdown.Item>
                                 {/* <Dropdown.Item name="done" onClick={this.props.taskSort}>{showDone}</Dropdown.Item> */}
 
                         </DropdownButton>
